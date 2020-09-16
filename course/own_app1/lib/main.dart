004291beba23a6fbb4c4504f986b31fe71e3e8a1
SatoShun <shun.sato1@gmail.com>
2020-09-16 09:26:11 +0000

    own app

---
+++ course/own_app1/lib/main.dart
@@ -0,0 +1,29 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:solution_01_hello_rectangle/Twitter.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hello Rectangle',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            children: <Widget>[
+              Icon(Icons.add),
+              Center(
+                child: Icon(Icons.cake),
+              ),
+              Icon(Icons.add),
+            ],
+          ),
+        ),
+        body: Twitter(),
+      ),
+    ),
+  );
+}
